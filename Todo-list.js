0614294d17a724f3f96f1a2f07e7c78f35060e6e
Asnith <121656672+Axnith@users.noreply.github.com>
2025-08-19 22:36:49 +0530

    Update Todo-list.js

--- Todo-list.js
+++ Todo-list.js
@@ -1,4 +1,5 @@
 let TodoList = JSON.parse(localStorage.getItem('TodoList')) || [];
+localStorage.setItem('TodoList',JSON.stringify(TodoList));
 function AddToList(){
   const inputName = document.querySelector('.js-input-name');
   const taskname = inputName.value;
@@ -37,6 +38,7 @@ function ClearAll(index){
   DisplayHTML();
 }
 DisplayHTML();
+
 
 
 
